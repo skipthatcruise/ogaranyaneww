7be80823f36b621f922237520727b10b7e599519
skipthatcruise <finndave15@gmail.com>
2026-01-20 11:37:33 +0100

    new stuff

---
+++ public/test.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = "Oyekan David";
+$age = 100;
+$date = 50;
+
+echo $name . "<br>"; 
+echo $age . "<br>";
+echo $date;
+
+
+$number = 30;
+if ($number%2==0){
+    echo "$number an even number";
+}
+else{
+    echo "$number an odd number";
+}
+
+
+$i = 1;
+while ($i<21){
+    echo $i . "<br>";
+    $i++ ;
+}
